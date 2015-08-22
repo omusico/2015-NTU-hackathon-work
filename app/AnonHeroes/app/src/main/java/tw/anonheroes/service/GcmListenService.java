c6d9d39ea9873cb8d23c4d209e6080dc3f8f37bb
Invictus <ivan055111@gmail.com>
2015-08-22 16:51:29 +0800

    add background

--- app/AnonHeroes/app/src/main/java/tw/anonheroes/service/GcmListenService.java
+++ app/AnonHeroes/app/src/main/java/tw/anonheroes/service/GcmListenService.java
@@ -1,5 +1,7 @@
 package tw.anonheroes.service;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -13,8 +15,8 @@ public class GcmListenService extends GcmListenerService {
 
     @Override
     public void onMessageReceived(String from, Bundle data) {
-        // TODO: GET NOTIFICATION
-        Log.i(TAG, from + ": " + data.getString("message"));
+
+        Log.i(TAG, from + ": " + data.getString("data"));
         super.onMessageReceived(from, data);
     }
 }
